e11ea9338853cfed0b99557e0cb7337f75d2772a
Canon11 <mrt014kzm@gmail.com>
2019-03-19 22:11:09 +0900

    implement category controller

--- controllers/categories.go
+++ controllers/categories.go
@@ -1,30 +1,100 @@
 package controllers
 
 import (
+	"bookapp/models"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
 
 func CategoryList(c *gin.Context) {
-	res := testResp{ID: 2, Name: "category_list"}
-	c.JSON(200, res)
+	client, err := models.NewClient()
+	if err != nil {
+		c.Error(err)
+	}
+
+	categories, err := client.ListCategory(c)
+	if err != nil {
+		c.Error(err)
+	}
+
+	c.JSON(200, categories)
 }
 
 func CategoryCreate(c *gin.Context) {
-	res := testResp{ID: 3, Name: "category_create"}
-	c.JSON(200, res)
+	client, err := models.NewClient()
+	if err != nil {
+		c.Error(err)
+	}
+
+	category := models.Category{
+		Name: c.PostForm("name"),
+	}
+	if err := client.CreateCategory(c, &category); err != nil {
+		c.Error(err)
+	}
+	c.JSON(200, nil)
 }
 
 func CategoryDetail(c *gin.Context) {
-	res := testResp{ID: 4, Name: "category_detail"}
-	c.JSON(200, res)
+	client, err := models.NewClient()
+	if err != nil {
+		c.Error(err)
+	}
+
+	paramID := c.Param("id")
+	categoryID, err := strconv.Atoi(paramID)
+	if err != nil {
+		c.Error(err)
+	}
+
+	category, err := client.GetBook(c, int64(categoryID))
+	if err != nil {
+		c.Error(err)
+	}
+
+	c.JSON(200, category)
 }
 
 func CategoryEdit(c *gin.Context) {
-	res := testResp{ID: 5, Name: "category_edit"}
+	client, err := models.NewClient()
+	if err != nil {
+		c.Error(err)
+	}
+
+	paramID := c.Param("id")
+	categoryID, err := strconv.Atoi(paramID)
+	if err != nil {
+		c.Error(err)
+	}
+
+	category := models.Category{
+		ID:   int64(categoryID),
+		Name: c.PostForm("name"),
+	}
+	res, err := client.EditCategory(c, &category)
+	if err != nil {
+		c.Error(err)
+	}
+
 	c.JSON(200, res)
 }
 
 func CategoryDelete(c *gin.Context) {
-	res := testResp{ID: 6, Name: "category_delete"}
-	c.JSON(200, res)
+	client, err := models.NewClient()
+	if err != nil {
+		c.Error(err)
+	}
+
+	paramID := c.Param("id")
+	categoryID, err := strconv.Atoi(paramID)
+	if err != nil {
+		c.Error(err)
+	}
+
+	if err = client.DeleteCategory(c, int64(categoryID)); err != nil {
+		c.Error(err)
+	}
+
+	c.JSON(200, nil)
 }
